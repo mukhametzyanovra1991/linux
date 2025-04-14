0e6d28d2ef9d0d6006c040c3b87efbf421a5c397
mukhametzyanovra1991 <165897433+mukhametzyanovra1991@users.noreply.github.com>
2025-04-14 09:59:23 +0000

    ad_to_domain.sh $1 - domain_user_name $2 - domain_name

--- AD/ad_to_domain.sh
+++ AD/ad_to_domain.sh
@@ -4,7 +4,7 @@
 apt -y install realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
 
 #join to active directory domain b17.local
-realm join -U mukhametzyanovra b17.local
+realm join -U $1 $2
 
 #enable auto-create user homedir
 bash -c "cat > /usr/share/pam-configs/mkhomedir" <<EOF
@@ -28,7 +28,7 @@ realm permit -g linux_adm
 #add linux_adm AD group to sudoers
 bash -c "cat > /etc/sudoers.d/linux_adm" <<EOF
 %linux_adm              ALL=(ALL)       ALL
-%linux_adm@b17.local    ALL=(ALL)       ALL
+%linux_adm@$2    ALL=(ALL)       ALL
 EOF
 
 #use_fully_qualified_names=False to login without domain name
