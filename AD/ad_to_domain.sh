a171dbcb3b740716d92c42a6f481818907861fd1
mukhametzyanovra1991 <165897433+mukhametzyanovra1991@users.noreply.github.com>
2025-04-14 09:43:19 +0000

    ad_to_domain.sh $1 - domain_user_name $2 - domain_name

---
+++ AD/ad_to_domain.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#install required packages
+apt -y install realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
+
+#join to active directory domain b17.local
+realm join -U mukhametzyanovra b17.local
+
+#enable auto-create user homedir
+bash -c "cat > /usr/share/pam-configs/mkhomedir" <<EOF
+Name: activate mkhomedir
+Default: yes
+Priority: 900
+Session-Type: Additional
+Session:
+        required                        pam_mkhomedir.so umask=0022 skel=/etc/skel
+EOF
+
+#enable mkhomedir
+pam-auth-update
+
+#restart sssd daemon
+sudo systemctl restart sssd
+
+#allow login to linux_adm AD group
+realm permit -g linux_adm
+
+#add linux_adm AD group to sudoers
+bash -c "cat > /etc/sudoers.d/linux_adm" <<EOF
+%linux_adm              ALL=(ALL)       ALL
+%linux_adm@b17.local    ALL=(ALL)       ALL
+EOF
+
+#use_fully_qualified_names=False to login without domain name
+sed -i.bak 's/use_fully_qualified_names = True/use_fully_qualified_names = False/g' /etc/sssd/sssd.conf
