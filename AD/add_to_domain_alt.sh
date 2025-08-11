b8c8dc18f304ef276932a78478334fe0a5d6f58c
mukhametzyanovra1991 <165897433+mukhametzyanovra1991@users.noreply.github.com>
2025-08-11 17:14:04 +0500

    Update add_to_domain_alt.sh

--- AD/add_to_domain_alt.sh
+++ AD/add_to_domain_alt.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+#install required packages
+apt-get update -y
+apt-get install task-auth-ad-sssd -y
+
+#set hostname
+hostnamectl set-hostname $1.$2
+
+#add to domain gkb5.local
+system-auth write ad $2 $1 $3 $4
+
+#roleadd 
+roleadd 'adm_linux' localadmins
